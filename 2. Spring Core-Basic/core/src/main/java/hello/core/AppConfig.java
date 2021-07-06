d4652d6bb206da32b46574609c69477d4f9ce06a
jimin3263 <dkdldsla0000@naver.com>
2021-07-06 20:52:29 +0900

    스프링으로 전환

--- 2. Spring Core-Basic/core/src/main/java/hello/core/AppConfig.java
+++ 2. Spring Core-Basic/core/src/main/java/hello/core/AppConfig.java
@@ -2,31 +2,40 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memoryRepository());
     }
 
-    private MemberRepository memoryRepository() {
+    @Bean
+    public MemberRepository memoryRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memoryRepository(),
                 discountPolicy());
     }
 
-    private DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
