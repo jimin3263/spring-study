aa040aa615db17262fa3a75ad32fbcd4f1c3c3c6
jimin3263 <dkdldsla0000@naver.com>
2021-07-26 01:55:07 +0900

    request mapping

---
+++ 4. Spring MVC-(1)/springmvc/src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
@@ -0,0 +1,38 @@
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+@RestController
+@Slf4j
+public class RequestHeaderController {
+
+    @RequestMapping("/headers")
+    public String headers(HttpServletResponse response,
+                          HttpServletRequest request,
+                          HttpMethod httpMethod,
+                          Locale locale,
+                          @RequestHeader("host") String host,
+                          @RequestHeader MultiValueMap<String, String> headerMap,
+                          @CookieValue(value = "myCookie", required = false) String cookie){
+
+        log.info("request={}", request);
+        log.info("response={}", response);
+        log.info("httpMethod={}", httpMethod);
+        log.info("locale={}", locale);
+        log.info("headerMap={}", headerMap);
+        log.info("header host={}", host);
+        log.info("myCookie={}", cookie);
+        return "ok";
+
+    }
+}
