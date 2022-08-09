d9325741651dd6c007d99cc41a6eaeb1ac1007a6
limjimin <dkdldsla0000@naver.com>
2022-08-10 01:18:45 +0900

    리플렉션

---
+++ 6. Spring Core-Advanced/proxy/src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
@@ -0,0 +1,81 @@
+package hello.proxy.jdkdynamic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+@Slf4j
+public class ReflectionTest {
+
+    @Test
+    void reflection() { //호출하는 메서드만 다르다.
+        Hello target = new Hello();
+
+        //공통 로직1 시작
+        log.info("start");
+        String result1 = target.callA();
+        log.info("result={}", result1);
+        //공통 로직1 종료
+
+        //공통 로직2 시작
+        log.info("start");
+        String result2 = target.callB();
+        log.info("result={}", result2);
+        //공통 로직2 종료
+    }
+
+    /**
+     * 메서드 직접 호출하는 부분이 Method로 대체될 수 있
+     */
+    @Test
+    void reflection1() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        //클래스 메타 정보
+        Class classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
+
+        Hello target = new Hello();
+        //callA 메서드 정보
+        Method methodCallA = classHello.getMethod("callA");
+        Object result1 = methodCallA.invoke(target); //획득한 메타정보로 실제 인스턴스의 메서드 호출
+        log.info("result1={}", result1);
+
+        //callB 메서드 정보
+        Method methodCallB = classHello.getMethod("callB");
+        Object result2 = methodCallB.invoke(target);
+        log.info("result2={}", result2);
+    }
+
+
+    @Test
+    void reflection2() throws Exception {
+        //클래스 메타 정보
+        Class classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
+
+        Hello target = new Hello();
+        Method methodCallA = classHello.getMethod("callA");
+        dynamicCall(methodCallA, target);
+
+        Method methodCallB = classHello.getMethod("callB");
+        dynamicCall(methodCallB, target);
+    }
+
+    private void dynamicCall(Method method, Object target) throws Exception {
+        log.info("start");
+        Object result = method.invoke(target); //추상화된 부분
+        log.info("result={}", result);
+    }
+
+    @Slf4j
+    static class Hello {
+        public String callA() {
+            log.info("callA");
+            return "A";
+        }
+
+        public String callB() {
+            log.info("callB");
+            return "B";
+        }
+    }
+}
