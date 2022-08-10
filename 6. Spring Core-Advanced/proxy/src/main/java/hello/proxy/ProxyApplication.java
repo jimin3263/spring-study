667259b9e322cfb563e6e9bbab5a5d5e77d0c88d
limjimin <dkdldsla0000@naver.com>
2022-08-10 18:41:03 +0900

    프록시 팩토리 적용

--- 6. Spring Core-Advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ 6. Spring Core-Advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -6,6 +6,8 @@ import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v2_dynamic_proxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamic_proxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -18,7 +20,9 @@ import org.springframework.context.annotation.Import;
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
-@Import(DynamicProxyFilterConfig.class)
+//@Import(DynamicProxyFilterConfig.class)
+//@Import(ProxyFactoryConfigV1.class)
+@Import(ProxyFactoryConfigV2.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의 , componentScan 시작 위치 지정 -> 버전 별 관리를 위함
 public class ProxyApplication {
 
