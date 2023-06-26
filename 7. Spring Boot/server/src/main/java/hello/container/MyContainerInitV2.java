843de3cce251b662e4c6e7b2c3d1eda14c2fa8cc
limjimin <dkdldsla0000@naver.com>
2023-06-27 01:02:54 +0900

    웹 서버와 서블릿 컨테이너

---
+++ 7. Spring Boot/server/src/main/java/hello/container/MyContainerInitV2.java
@@ -0,0 +1,28 @@
+package hello.container;
+
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.HandlesTypes;
+
+import java.util.Set;
+
+@HandlesTypes(AppInit.class)
+public class MyContainerInitV2 implements ServletContainerInitializer {
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+        System.out.println("MyContainerInitV2.onStartup");
+        System.out.println("c = " + c + ", ctx = " + ctx);
+
+        for (Class<?> appInitClass : c) {
+            try {
+                //new AppInitV1Servlet()과 같은 코드
+                AppInit appInit = (AppInit) appInitClass.getDeclaredConstructor().newInstance();
+                appInit.onStartup(ctx);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+}
